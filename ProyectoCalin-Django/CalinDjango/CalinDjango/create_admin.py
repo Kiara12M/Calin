84552be378726c282c5f99d45b6b4251fbce6c8d
kiara12M <kiaramartinezromero121@gmail.com>
2026-05-19 03:13:57 +0200

    fix create superuser args

--- ProyectoCalin-Django/CalinDjango/CalinDjango/create_admin.py
+++ ProyectoCalin-Django/CalinDjango/CalinDjango/create_admin.py
@@ -1,16 +1,14 @@
 import django
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CalinDjango.settings')
-
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CalinDjango.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
 U = get_user_model()
 if not U.objects.filter(email='admin@calin.com').exists():
-    U.objects.create_superuser('admin@calin.com', 'admin@calin.com', 'Admin1234!')
+    U.objects.create_superuser(email='admin@calin.com', password='Admin1234!')
     print("Superusuario creado")
 else:
     print("Ya existe")
