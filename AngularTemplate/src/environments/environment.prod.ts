31817acabb8381b0694bccae052c8ab4a9118a60
kiara12M <kiaramartinezromero121@gmail.com>
2026-05-19 02:01:14 +0200

    fix api url production

--- AngularTemplate/src/environments/environment.prod.ts
+++ AngularTemplate/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    // REEMPLAZAR ESTA URL CON LA URL PROPORCIONADA POR RENDER CUANDO DESPLIEGUES EL BACKEND
     apiURL: 'https://calin-5z04.onrender.com/api/',
-}
+};
